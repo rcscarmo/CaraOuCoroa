2c3263be3c87470dea23770072e24a74414622d8
Richardson Christian <rcsc3w@yahoo.com.br>
2026-02-09 16:05:38 -0300

    moedas recortadas

---
+++ settings.py
@@ -0,0 +1,7 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+IMAGES_DIR = os.path.join(BASE_DIR, 'images')
+MOEDAS_DIR = os.path.join(IMAGES_DIR, 'moedas')
+
+MOEDA_ATUAL = 'real'
